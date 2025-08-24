d519c88e2f2e47bd749465c67b1a9e310b7571fd
ubuntusitubondo <russ@earthen.io>
2025-08-24 17:33:51 +0700

    new signup 4

--- en/signup-1.php
+++ en/signup-1.php
@@ -66,9 +66,9 @@ https://github.com/gea-ecobricks/buwana/-->
    $page_key = str_replace('-', '_', $page); // e.g. 'signup-1' → 'signup_1'
    ?>
 
-<div class="page-panel-group">
+<div class="login-panel-group">
     <div id="form-submission-box" class="landing-page-form">
-        <div class="form-container" style="margin:auto;">
+        <div class="form-container">
             <div id="top-page-image"
                  class="top-page-image"
                  data-light-img="<?= htmlspecialchars($app_info[$page_key . '_top_img_light']) ?>"
@@ -90,8 +90,7 @@ https://github.com/gea-ecobricks/buwana/-->
                         maxlength="255"
                         required
                         placeholder=" " />
-                 <label for="first_nam        <div class="form-container" style="margin:auto;">
-e" data-lang-id="003-firstname">What's your first name?</label>
+                 <label for="first_name" data-lang-id="003-firstname">What's your first name?</label>
                  <!-- ERRORS -->
                  <div id="maker-error-required" class="form-field-error" data-lang-id="000-field-required-error">This field is required.</div>
                  <div id="maker-error-long" class="form-field-error" data-lang-id="000-name-field-too-long-error">The name is too long. Max 255 characters.</div>
