44b005d96994a207698d05b658b98bd9531667a1
ubuntusitubondo <russ@earthen.io>
2025-08-27 17:52:22 +0700

    new signup 4

--- en/signup-7.php
+++ en/signup-7.php
@@ -71,7 +71,7 @@ https://github.com/gea-ecobricks/buwana/-->
 <?php require_once ("../includes/signup-7-inc.php");?>
 
 <div class="page-panel-group">
-    <div id="form-submission-box" class="landing-page-form" style="min-height:calc( 100vh - 54px)">
+    <div id="form-submission-box" class="landing-page-form">
         <div class="form-container" style="box-shadow: #0000001f 0px 5px 20px;margin:auto;">
 
       <div id="top-page-image" class="top-page-image"></div>
@@ -95,6 +95,7 @@ https://github.com/gea-ecobricks/buwana/-->
         </p>
     </div>
   </div>
+</div>
 </div>
 
 <?php require_once ("../footer-2025.php"); ?>
