ac28fe24d62ea6813192f5292f8048fcd6566af5
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-09-08 16:39:00 +0700

    Add goodbye page

---
+++ en/goodbye.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+
+require_once '../buwanaconn_env.php';
+require_once '../fetch_app_info.php';
+
+$lang = basename(dirname($_SERVER['SCRIPT_NAME']));
+$page = 'goodbye';
+$version = '0.1';
+$lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
+?>
+
+<!DOCTYPE html>
+<html lang="<?= htmlspecialchars($lang) ?>">
+<head>
+  <meta charset="UTF-8">
+<?php require_once ("../includes/goodbye-inc.php");?>
+
+<!-- PAGE CONTENT -->
+<div id="top-page-image" class="regen-top top-page-image"></div>
+
+<div id="form-submission-box" class="landing-page-form">
+    <div class="form-container">
+
+      <div style="text-align:center;width:100%;margin:auto;">
+
+        <h1 data-lang-id="001-good-bye">Goodbye!</h1>
+        <p data-lang-id="002-successfuly-deleted">Your account has been successfully deleted.</p>
+        <p data-lang-id="003-change-mind">If you change your mind, you can <a href="signup-1.php">create a new account</a> anytime.</p>
+      </div>
+    </div>
+</div>
+
+<?php require_once ("../footer-2025.php");?>
+
+</body>
+</html>
