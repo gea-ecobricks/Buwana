376e06c7205c06142d6a9d4f34bc0f48d78afb5f
ubuntusitubondo <russ@earthen.io>
2025-11-03 11:49:39 +0700

    Fixing legcy conifrm email system

---
+++ meta/feedback-en.php
@@ -0,0 +1,20 @@
+<title>Your feedback please!</title>
+
+<meta name="keywords" content="bug report, suggestions, feedback, conversations, development team, feature requests, help, support, technical support, assistance, contact, contact us">
+<meta name="description" content="Share bugs, suggestions, requests and everything esle with the Buwana development team.">
+<meta name="author" content="Global Ecobrick Alliance">
+<meta name="last-modified" content="' . $lastModified . '">
+<meta name="revised" content="' . $lastModified . '">
+
+<!-- Facebook Open Graph Tags for social sharing-->
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+<meta property="og:url"           content="https://buwana.ecobricks.org/' . $lang . '/' . $page . '">
+<meta property="og:type"          content="app">
+<meta property="og:title"         content="Your feedback please!">
+<meta property="og:description"   content="Share bugs, suggestions, requests and everything esle with the Buwana development team." >
+<meta property="og:image"         content="https://buwana.ecobricks.org/pngs/bug-report-banner-1000px.png" >
+<meta property="fb:app_id"        content="1781710898523821" >
+<meta property="og:image:width"   content="1000" >
+<meta property="og:image:height"  content="373" >
+<meta property="og:image:alt"     content="A bird holding a bug to share with everyone.">
+<meta property="og:locale"        content="en_GB">
--- meta/profile-en.php
+++ meta/profile-en.php
@@ -1,18 +1,18 @@
 <title>Account Profile | Settings</title>
 
 <meta name="keywords" content="user profile, user settings, account, update profile, gobrik, save settings, set country, language, watershed">
-<meta name="description" content="Manage your GoBrik and Buwana Account settings">
+<meta name="description" content="Manage your personal Buwana account settings">
 <meta name="author" content="Global Ecobrick Alliance">
 <meta name="last-modified" content="' . $lastModified . '">
 <meta name="revised" content="' . $lastModified . '">
 
 <!-- Facebook Open Graph Tags for social sharing-->
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" >
-<meta property="og:url"           content="https://www.gobrik.com/' . $lang . '/' . $page . '">
+<meta property="og:url"           content="https://buwana.ecobricks.org/' . $lang . '/' . $page . '">
 <meta property="og:type"          content="app">
-<meta property="og:title"         content="Account Profile | GoBrik">
-<meta property="og:description"   content="Manage your GoBrik and Buwana Account settings" >
-<meta property="og:image"         content="https://www.gobrik.com/images/social-banner-1200px.png" >
+<meta property="og:title"         content="Account Profile">
+<meta property="og:description"   content="Manage your personal Buwana account settings" >
+<meta property="og:image"         content="https://buwana.ecobricks.org/images/social-banner-1200px.png" >
 <meta property="fb:app_id"        content="1781710898523821" >
 <meta property="og:image:width"   content="1200" >
 <meta property="og:image:height"  content="1000" >
