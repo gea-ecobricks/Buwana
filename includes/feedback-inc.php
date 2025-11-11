8902480548fcb563a4276a53317421234e397dae
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-11-11 14:13:40 +0700

    Refine support chat modal and navigation

--- includes/feedback-inc.php
+++ includes/feedback-inc.php
@@ -1,5 +1,6 @@
 <?php require_once ("../meta/$page-$lang.php");?>
 
+<link rel="stylesheet" href="../styles/jquery.dataTables.css">
 <link rel="stylesheet" type="text/css" href="../styles/cs_system.css?v=<?php echo ($version); ?>">
 
 <?php require_once ("../header-2025.php");?>
